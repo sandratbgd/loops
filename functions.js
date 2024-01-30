8f930780d13371a7ff159212d96f4a3f6f76e1ee
Sandra Tierraseca <sandra.tierraseca@twtspain.com>
2024-01-30 20:46:00 +0100

    30 de enero

---
+++ functions.js
@@ -0,0 +1,8 @@
+
+    let ejArray1 = Array.from('hola') // [ 'h', 'o', 'l', 'a' ]
+                console.log(ejArray1);
+
+    let ejFuncion = Array.from('hola', el => el.toUpperCase()) // [ 'H', 'O', 'L', 'A' ]
+                console.log(ejFuncion);
+
+    
